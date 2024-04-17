26fb1b3e530abf6e886167a4c8c41a09329173da
Doschgo <doschgo@gmail.com>
2024-04-17 23:32:49 +0200

    add leaflet; add mapTiles;

--- resources/js/Pages/index.tsx
+++ resources/js/Pages/index.tsx
@@ -1,11 +1,38 @@
 import { Link, Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
 import React from 'react';
+import { MapContainer, TileLayer, useMap, Marker, Popup, LayersControl, LayersControlProps } from 'react-leaflet'
+const center = [0, 0]
 
-export default function Welcome({laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
+
+
+export default function Index({laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
+
 
 
     return (
-        <h1>dev</h1>
+        <section>
+            <MapContainer center={[0, 0.0]} zoom={5} scrollWheelZoom={false}>
+
+
+<LayersControl position="topright">
+      <LayersControl.BaseLayer name="Atlas">
+      <TileLayer
+    url="mapStyles/styleAtlas/{z}/{x}/{y}.jpg"
+  />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="Grid">
+      <TileLayer
+    url="mapStyles/styleGrid/{z}/{x}/{y}.png"
+  />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer checked name="Satelite">
+      <TileLayer
+    url="mapStyles/styleSatelite/{z}/{x}/{y}.jpg"
+  />
+      </LayersControl.BaseLayer>
+    </LayersControl>
+</MapContainer>
+        </section>
     );
 }
